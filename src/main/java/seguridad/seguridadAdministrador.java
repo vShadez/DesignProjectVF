5fb5440f003fa48b886be7f6d3772e5745504344
Fabian2410RN <fabianriosig26@gmail.com>
2022-06-13 17:04:52 -0600

    1

--- src/main/java/seguridad/seguridadAdministrador.java
+++ src/main/java/seguridad/seguridadAdministrador.java
@@ -11,5 +11,4 @@ public class seguridadAdministrador {
     public static boolean inicioSesionAdministrador(String pUsuario, String pPin){
         return "JSF".equals(pUsuario) && "123".equals(pPin);
     }
-    
 }
