a5b74f8bc8d1d0d7d9eaf5c11a44d81b937e65a6
Fabian2410RN <fabianriosig26@gmail.com>
2022-06-14 21:56:49 -0600

    3

--- src/main/java/seguridad/seguridadAdministrador.java
+++ src/main/java/seguridad/seguridadAdministrador.java
@@ -6,7 +6,7 @@ import javax.swing.JOptionPane;
  *
  * @author estadm
  */
-public class seguridadAdministrador {
+public class SeguridadAdministrador {
     
     public static boolean iniciarSesionAdministrador(String pUsuario, String pPin){
         return "JSF".equals(pUsuario) && "123".equals(pPin);
